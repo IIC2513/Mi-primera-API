d4dc4696b385079dbd4b0c5d42ef5c839aa9edcb
Antonio Ossa Guerra <aaossa@uc.cl>
2024-04-16 17:07:17 -0400

    :sparkles: Update movies router to fetch records
    The first query that will return the complete collection of movies on the
    `/movies` endpoint
    
    Signed-off-by: Antonio Ossa Guerra <aaossa@uc.cl>

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,9 +1,16 @@
 // src/routes/movies.js
 const Router = require('koa-router');
 const router = new Router();
+const { Movie } = require('../models');
 
-router.get('/', (ctx) => {
-  ctx.body = 'GET /movies';
+router.get('/', async (ctx) => {
+  try {
+    const movies = await Movie.findAll();
+    ctx.body = movies;
+  } catch (error) {
+    console.log(error);
+    ctx.throw(404);
+  }
 });
 
 module.exports = router;
