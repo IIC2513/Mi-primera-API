9a699f162967f5e563688a2ea86f79ff9b10e456
Antonio Ossa Guerra <aaossa@uc.cl>
2024-04-16 14:33:00 -0400

    :sparkles: Update server to use our main router
    Now, the existing router routes will be reachable through the server
    
    Signed-off-by: Antonio Ossa Guerra <aaossa@uc.cl>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 // src/index.js
 const Koa = require('koa');
 const Logger = require('koa-logger');
+const router = require('./routes');
 
 const app = new Koa();
 
 app.use(Logger());
 
-app.use((ctx, next) => {
-  ctx.body = 'Hola mundo';
-});
+app.use(router.routes());
 
 app.listen(3000);
